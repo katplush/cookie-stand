ae4f9b8534691a46130b795ceeed5a58c05bc3fc
Katerina Plushko <katplush@yahoo.com>
2018-01-25 19:11:16 -0800

    fixed object lits; added randCustpHour and totalSold keys

--- js/app.js
+++ js/app.js
@@ -4,40 +4,45 @@ var pike = {
   storeLoc: '1st and Pike',
   minNumCust: 23,
   maxNumCust: 65,
-  avgNumCook: 6.3,
-  hoursOpen: 14,
+  avgSold: 6.3,
+  randCustpHour: [],
+  totalSold: [],
 };
 
 var seaTac = {
   storeLoc: 'SeaTac Airport',
   minNumCust: 3,
   maxNumCust: 24,
-  avgNumCook: 1.2,
-  hoursOpen: 14,
+  avgSold: 1.2,
+  randCustpHour: [],
+  totalSold: [],
 };
 
 var seaCent = {
   storeLoc: 'Seattle Center',
   minNumCust: 11,
   maxNumCust: 38,
-  avgNumCook: 3.7,
-  hoursOpen: 14,
+  avgSold: 3.7,
+  randCustpHour: [],
+  totalSold: [],
 };
 
 var capHill = {
   storeLoc: 'Capitol Hill',
   minNumCust: 20,
   maxNumCust: 38,
-  avgNumCook: 2.3,
-  hoursOpen: 14,
+  avgSold: 2.3,
+  randCustpHour: [],
+  totalSold: [],
 };
 
 var alki = {
   storeLoc: 'Alki',
   minNumCust: 2,
   maxNumCust: 16,
-  avgNumCook: 4.6,
-  hoursOpen: 14,
+  avgSold: 4.6,
+  randCustpHour: [],
+  totalSold: [],
 };
 
 var hoursOpen = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
